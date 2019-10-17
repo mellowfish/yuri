6443e6845b6a41ffd39602095ac4ec65ad4e8c24
Christopher Coleman <christopher.coleman@daveramsey.com>
2019-10-17 10:57:21 -0500

    puts => console.log

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -4,6 +4,23 @@ class Translation
   attr_accessor :input_code, :input_language, :output_language
 
   def output_code
-    input_code
+    input_code.to_s.split("\n").map do |input_line|
+      translate_line(input_line)
+    end.join("\n")
+  end
+
+  MAPPINGS = {
+    /puts "(?<string>[^"]+?)"/ => {
+      code: "console.log('%<string>s');",
+      keys: %w(string)
+    }
+  }.freeze
+
+  def translate_line(line)
+    MAPPINGS.each do |regex, mapping_data|
+      regex.match(line) do |match_data|
+        return format(mapping_data[:code], match_data.named_captures.slice(*mapping_data[:keys]).symbolize_keys)
+      end
+    end
   end
 end
