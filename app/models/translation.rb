34cf5189a5b5027a7e092e68bcea71f9899e243f
Christopher Coleman <christopher.coleman@daveramsey.com>
2019-10-17 11:27:18 -0500

    generalize string translation, add Array#sum

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -4,10 +4,33 @@ class Translation
   MAPPINGS = {
     ruby: {
       js: {
-        /^puts[ \(]"(?<string>[^"]+?)"[\)]?$/ => {
-          code: "console.log('%<string>s');",
-          keys: %w(string)
-        }
+        expressions: {
+          /"(?<string>[^"]+?)"/ => {
+            code: "'%<string>s'",
+            keys: %w(string)
+          },
+        },
+        lines: {
+          /^puts[ \(](?<expression>.+?)[\)]?$/ => {
+            code: "console.log(%<expression>s)",
+            keys: %w(expression)
+          },
+          /^(?<variable_name>\w+)[ ]?=[ ]?(?<expression>.+?)$/ => {
+            code: "var %<variable_name>s = %<expression>s",
+            keys: %w(variable_name expression)
+          },
+          /^(?<expression>.+?)\.sum$/ => {
+            code: <<~JS.chomp,
+              %<expression>s.reduce((a, b) => a + b);
+            JS
+            keys: %w(variable_name expression)
+          },
+        },
+      }
+    },
+    js: {
+      ruby: {
+        # TODO
       }
     }
   }.freeze
@@ -24,13 +47,33 @@ class Translation
     MAPPINGS.fetch(input_language.to_sym, {}).fetch(output_language.to_sym, {})
   end
 
+  def expression_mappings
+    mappings.fetch(:expressions, {})
+  end
+
+  def line_mappings
+    mappings.fetch(:lines, {})
+  end
+
   def translate_line(line)
-    mappings.each do |regex, mapping_data|
+    expression_mappings.each do |regex, mapping_data|
       regex.match(line) do |match_data|
-        return format(mapping_data[:code], match_data.named_captures.slice(*mapping_data[:keys]).symbolize_keys)
+        line[regex] = format(mapping_data[:code], match_data.named_captures.slice(*mapping_data[:keys]).symbolize_keys)
       end
     end
 
-    line
+    line_mappings.each do |regex, mapping_data|
+      regex.match(line) do |match_data|
+        line = format(mapping_data[:code], match_data.named_captures.slice(*mapping_data[:keys]).symbolize_keys)
+      end
+    end
+
+    if line.blank?
+      ""
+    elsif line.last == ";"
+      line
+    else
+      "#{line};"
+    end
   end
 end
